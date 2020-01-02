6af4796fcbe301e4d3fda58a744bac619134ccee
C Bonifacio <cmtbonifacio@gmail.com>
2020-01-02 09:16:54 -0700

    Update to include Map of Recipients

---
+++ list.js
@@ -0,0 +1,479 @@
+var recipientSchools = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+                "type": "Point",
+                "coordinates": [124.839088, 11.2071989]
+            },
+            "properties": {
+                "popupContent": "Alangalang 1 Central School"
+            }
+      },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9505889, 11.3160188]
+            },
+            "properties": {
+                "popupContent": "B. Bulante Elementary School"
+            }
+        },
+        {
+           "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.5081518, 11.4731539]
+            },
+            "properties": {
+                "popupContent": "Balangkayan National High School"
+            }
+
+        },
+        {
+           "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9790743, 11.2465049]
+            },
+            "properties": {
+                "popupContent": "Binongtoan Central School"
+            }
+
+        },
+        {
+          "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.5101663, 11.656918]
+            },
+            "properties": {
+                "popupContent": "Binongtoan Central School"
+            }
+
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [122.9120052, 11.2050389]
+            },
+            "properties": {
+                "popupContent": "Cabacungan Elementary School"
+            }
+
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.4475896, 11.9978016]
+            },
+            "properties": {
+                "popupContent": "Can-Avid Central Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.7311465,11.3166666]
+            },
+            "properties": {
+                "popupContent": "Canomantag Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [122.8881595,9.8526007]
+            },
+            "properties": {
+                "popupContent": "Central Philippine State University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [126.1893943,9.2744789]
+            },
+            "properties": {
+                "popupContent": "Cortes Academy"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.6383918,11.1508664]
+            },
+            "properties": {
+                "popupContent": "Easter Samar State University, Salcedo Campus"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.4397052,11.6605334]
+            },
+            "properties": {
+                "popupContent": "Eastern Samar State University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.5049372,11.4901549]
+            },
+            "properties": {
+                "popupContent": "Eastern Samar State University, Maydolong Campus"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9951815,11.2401554]
+            },
+            "properties": {
+                "popupContent": "Eastern Visayas State University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [1124.7337431,11.3250214]
+            },
+            "properties": {
+                "popupContent": "Barugo II Central Elementary School (Ferrer)"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.7124158,11.0258611]
+            },
+            "properties": {
+                "popupContent": "Guiuan East Central Elementary School"
+            }
+		},		
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9823357,11.236406]
+            },
+            "properties": {
+                "popupContent": "Judge Antonio R. Montilla Sr Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.4217222,11.4673318]
+            },
+            "properties": {
+                "popupContent": "Lapgap National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.5422004,11.4129749]
+            },
+            "properties": {
+                "popupContent": "Llorente National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0141653,11.1985468]
+            },
+            "properties": {
+                "popupContent": "Manlurip Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0053697,11.195768]
+            },
+            "properties": {
+                "popupContent": "Marasbaras National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.4953496,11.5041178]
+            },
+            "properties": {
+                "popupContent": "Maydolong National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.7311465,11.3166666]
+            },
+            "properties": {
+                "popupContent": "Minuhang Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9473096,11.331567]
+            },
+            "properties": {
+                "popupContent": "Northern Tacloban City National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.4718585,11.5311591]
+            },
+            "properties": {
+                "popupContent": "Omawas Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9302822,12.5659111]
+            },
+            "properties": {
+                "popupContent": "Pambujan National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [120.982025,14.5877295]
+            },
+            "properties": {
+                "popupContent": "Philippine Normal University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0036289,11.2424729]
+            },
+            "properties": {
+                "popupContent": "Rizal Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0003947,11.216022]
+            },
+            "properties": {
+                "popupContent": "Sagkahan National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.000808,11.2339648]
+            },
+            "properties": {
+                "popupContent": "San Fernando Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0201727,11.205038]
+            },
+            "properties": {
+                "popupContent": "San Jose Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.8850739,11.1318849]
+            },
+            "properties": {
+                "popupContent": "San Jose National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.8283147,11.294691]
+            },
+            "properties": {
+                "popupContent": "San Miguel Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.7609127,11.300514]
+            },
+            "properties": {
+                "popupContent": "Santarin Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [122.1414069,7.1079087]
+            },
+            "properties": {
+                "popupContent": "Sapa Seco Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9775376,10.3909631]
+            },
+            "properties": {
+                "popupContent": "Southern Leyte State University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [123.2944824,7.8383054]
+            },
+            "properties": {
+                "popupContent": "Sta. Barbara Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.0978152,9.916667]
+            },
+            "properties": {
+                "popupContent": "Sta. Catalina Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.6555559,12.036903]
+            },
+            "properties": {
+                "popupContent": "Sta. Margarita National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.9726064,11.2520621]
+            },
+            "properties": {
+                "popupContent": "Sto. Nino Elementary School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0038444,11.2451558]
+            },
+            "properties": {
+                "popupContent": "Sto. Nino SPED Center"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0139239,11.10867]
+            },
+            "properties": {
+                "popupContent": "Tanauan I Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0119713,11.1052379]
+            },
+            "properties": {
+                "popupContent": "Tanauan II Central School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0119659,11.1052432]
+            },
+            "properties": {
+                "popupContent": "Tanauan National High School"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [122.0071957,10.791423]
+            },
+            "properties": {
+                "popupContent": "University of Antique, State University"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [125.0057841,11.2474385]
+            },
+            "properties": {
+                "popupContent": "UP Tacloban College"
+            }
+		},
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [124.8441661,11.638827]
+            },
+            "properties": {
+                "popupContent": "Zumarraga National High School"
+            }
+		}
+    ]
+};
